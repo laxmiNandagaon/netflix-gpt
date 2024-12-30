9194b77ead166c564bbef6e9fc45ccd59aa781ff
Laxmi <nandagaonlaxmi12@gmail.com>
2024-12-30 14:38:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-bold text-3xl text-green-400">
+     
+          Learn React
+       
+    </div>
+  );
+}
+
+export default App;
